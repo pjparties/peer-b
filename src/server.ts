47e34b62e5ada1899df3c8ac3c4decb3d2752fd8
parth <parthhjuneja@gmail.com>
2024-05-05 17:04:09 +0530

    at least this is hosting

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,39 @@
-import { App } from '@/app';
-import { AuthRoute } from '@routes/auth.route';
-import { UserRoute } from '@routes/users.route';
-import { ValidateEnv } from '@utils/validateEnv';
+import express from "express";
+import cors from "cors";    
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-ValidateEnv();
+const app = express();
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+);
+app.use(
+    express.json({
+        limit: "50kb",
+    })
+);
 
-const app = new App([new UserRoute(), new AuthRoute()]);
 
-app.listen();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('hello', 'world');
+});
+
+httpServer.listen(8000, () => console.log('Server is running on port 8000'));
